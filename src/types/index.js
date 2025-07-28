df3fe7989c080824ff7bad355aaf87a97ce75e4b
Dmitriy Istomin <distomin+git@pm.me>
2025-07-28 21:35:50 +0900

    delete .JS build outputs, commit .TS only

--- src/types/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * Barrel file for clean imports of all types
- */
-export { ConversationStatus, isConversationCompleted, isFinalSegment, } from './conversation';
-export { DeepGramErrorCode, isDeepGramError, isDeepGramResults, isApiError, isApiSuccess, isApiLoading, } from './api';
-export { getDefaultSettings, validateSettings } from './settings';
